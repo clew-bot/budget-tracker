d981977aaf1860a1d5acab5fb1d94f93bf732fbe
Chad Lew <clew@chads-imac.home>
2021-01-28 19:08:10 -0800

    debug server

--- server.js
+++ server.js
@@ -1,36 +1,28 @@
-//NPM Packages
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+
+const PORT = 3000;
+
 const app = express();
 
-
-//MiddleWare
 app.use(logger("dev"));
+
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
-// Mongoose
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGODB_URI, {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
-// Ports and Routes
-const PORT = process.env.PORT || 8080;
+// routes
 app.use(require("./routes/api.js"));
 
-// Listener
-mongoose.connection
-.once("open", () => console.log("Connected to + Mongoose")
-  .on("error", (error) => {
-    console.log("Your Error: ", error);
-  }));
 app.listen(PORT, () => {
-  console.log("APP RUNNING ON PORT ") + PORT;
+  console.log(`App running on port ${PORT}!`);
 });
